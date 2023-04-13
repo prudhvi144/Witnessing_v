e081c672265fde2896d38be87df2f2a165fa9b91
prudhvi144 <prudhvi9@bu.edu>
2023-04-12 22:09:44 -0400

    commit message

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='implimentationof vision transformers + contrastive learning',
+    author='prudhvi',
+    license='MIT',
+)
